ebd4c79b934af13f29ccbb2872e1ad19b406bcc5
Owen Dresner <owendrez@gmail.com>
2022-05-23 18:16:49 -0400

    commit tree 3b7ab1c2e41acedf647dbbceadbcde34efe178c1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = "bob"
+var bestCustomer
+const leastFavoriteCustomer= "bob"
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+     bestCustomer="not bob"
+    
+}
+function overwriteBestCustomer(customer){
+    bestCustomer=customer
+
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="not bob"
+}
